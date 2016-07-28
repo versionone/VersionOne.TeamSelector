f8428b04a9f9284b5b6e7ba1a4bea3f37140fe5b
Chris Draper <chrisd9809@gmail.com>
2016-07-28 15:04:05 -0400

    S-1 Add basic TeamCard React component and make it render in index.html

--- js/index.jsx
+++ js/index.jsx
@@ -1,1 +1,9 @@
-document.write("Webpack for the win!");
+/** @jsx React.DOM */
+
+'use strict'
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+var TeamCard = require('./TeamCard.jsx')
+
+ReactDOM.render(<TeamCard />, document.getElementById("content"));
---
+++ js/teamCard.jsx
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+'use strict'
+
+var React = require('react')
+
+var TeamCard = React.createClass({
+
+    render: function() {
+        return (
+            <div className="teamCard">This is a team card</div>
+        );
+    }
+});
+
+module.exports = TeamCard;
