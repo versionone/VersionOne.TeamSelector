4eca0d6e40ac0f3df56cd1b81731d682a9aa7ff6
Chris Draper <chrisd9809@gmail.com>
2016-07-28 17:36:07 -0400

    S-2 WIP - need to use Redux to handle state change

--- js/AddTeamCardButton.jsx
+++ js/AddTeamCardButton.jsx
@@ -6,6 +6,10 @@ var React = require('react')
 var TeamCard = require('./TeamCard.jsx')
 
 var AddTeamCardButton = React.createClass({
+    handleClick() {
+        {/*call something to start a chain reaction to change redux state here*/}
+    },
+
     render: function() {
         return (
             <div className="addButton">
