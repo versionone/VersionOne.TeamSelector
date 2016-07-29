fd497c6ba807e56fe7ba9aa2686e3e10618034b0
Chris Draper <chrisd9809@gmail.com>
2016-07-29 17:22:55 -0400

    S-2 Delete index.jsx and make TeamDisplay.jsx the webpack entry point

--- js/index.jsx
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-'use strict'
-
-var React = require('react')
-var ReactDOM = require('react-dom')
-var TeamDisplay = require('./TeamDisplay.jsx')
-
-ReactDOM.render(<TeamDisplay />, document.getElementById("content"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './js/index.jsx',
+  entry: './js/TeamDisplay.jsx',
   output: {
     filename: 'bundle.js'   
   },
