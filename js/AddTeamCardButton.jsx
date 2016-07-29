c6df420847fa17d5259dfe50d30052ac44bd7987
Chris Draper <chrisd9809@gmail.com>
2016-07-29 18:46:42 -0400

    S-2 Make AddTeamCardButton a class that extends React.Component so it can change the state of TeamDisplay

--- js/AddTeamCardButton.jsx
+++ js/AddTeamCardButton.jsx
@@ -1,16 +1,27 @@
 var React = require('react')
 var TeamCard = require('./TeamCard.jsx')
 
-var AddTeamCardButton = React.createClass({
-
-    render: function() {
+class AddTeamCardButton extends React.Component {
+    constructor() {
+        super();
+        this.props = {
+            teamData: React.PropTypes.array.isRequired
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    render() {
         return (
             <div className="addButton">
                 Add Team Card
             </div>
         );
     }
+    handleClick() {
+        var nextRank = teamData.length + 1;
+        var newName = "New Team " + nextRank;
+        document.write("Button was clicked");
+        //teamData.push({"teamName":newName, "rank":nextRank});
+    }
+}
 
-});
-
-module.exports = AddTeamCardButton;
+export default AddTeamCardButton;
