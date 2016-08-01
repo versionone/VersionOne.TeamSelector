1ab90eabf21fb3ab4b8e43338110ab973b6339ce
Chris Draper <chrisd9809@gmail.com>
2016-07-31 22:33:52 -0400

    S-2 Change the render method of TeamCardList to render out teamData into an array of TeamCard components
    - Team Card also requires a key to allow react to track the elements in the array

--- js/TeamCardList.jsx
+++ js/TeamCardList.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import TeamCard from './TeamCard.jsx';
 
-var TeamCardList = React.createClass({
-
-    propTypes: {
-        teamData: React.PropTypes.array.isRequired
-    },
-
-    render: function() {
+class TeamCardList extends React.Component {
+    constructor(props) {
+        super(props);
+        props = {
+            teamData: React.PropTypes.array.isRequired
+        }
+    }
+    render() {
         const {
             teamData
         } = this.props;
+        var teamDataRender = [];
+        for (var i = 0; i < teamData.length; i++) {
+            teamDataRender.push(
+                <TeamCard key={teamData[i].teamName}>
+                    {teamData[i].teamName}
+                </TeamCard>);
+        }
         return (
             <div className="teamCardList">
-                {teamData.map(function(data) {
-                    return <TeamCard key={data.teamName}>{data.teamName}</TeamCard>;
-                })}
+                {teamDataRender}
             </div>
         );
     }
-
-});
+}
 
 module.exports = TeamCardList;
