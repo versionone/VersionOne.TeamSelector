ae3bff95f1bbf5b7a07c1401a14114e018758645
Chris Draper <chrisd9809@gmail.com>
2016-08-04 13:43:54 -0400

    S-6 Add functionality to space out TeamCard color every two colors

--- js/TeamCardList.jsx
+++ js/TeamCardList.jsx
@@ -8,10 +8,13 @@ class TeamCardList extends React.Component {
     constructor(props) {
         super(props);
     }
-    findColor(colorArray, prevColor) {
+    findColor(colorArray, prevColor1, prevColor2) {
         var newColorArray = colorArray;
-        if (prevColor != null) {
-            newColorArray = _.without(newColorArray, prevColor);
+        if (prevColor1 != null) {
+            newColorArray = _.without(newColorArray, prevColor1);
+        }
+        if (prevColor2 != null) {
+            newColorArray = _.without(newColorArray, prevColor2);
         }
         var color = newColorArray[Math.floor(Math.random() * newColorArray.length)];
         return color;
@@ -23,10 +26,11 @@ class TeamCardList extends React.Component {
             closeTeamCard
         } = this.props;
         var teamDataRender = [];
-        var cardColor, prevColor;
+        var cardColor, prevColor1, prevColor2;
         for (var i = 0; i < teamData.length; i++) {
-            cardColor = this.findColor(colorArray, prevColor);
-            prevColor = cardColor;
+            cardColor = this.findColor(colorArray, prevColor1, prevColor2);
+            prevColor2 = prevColor1;
+            prevColor1 = cardColor;
             teamDataRender.push(
                 <TeamCard
                     key={i}
