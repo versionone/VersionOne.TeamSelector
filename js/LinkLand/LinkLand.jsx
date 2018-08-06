0fa3b8f63964353e99598f3703a31f93dbb0a8ed
Kevin Marsh <kmarsh@collab.net>
2018-08-06 10:18:27 -0400

    ability to save a link (or other text)

--- js/LinkLand/LinkLand.jsx
+++ js/LinkLand/LinkLand.jsx
@@ -4,16 +4,28 @@ import _ from 'underscore';
 class LinkLand extends React.Component {
     constructor(props) {
         super(props);
+        this.saveLink = this.saveLink.bind(this)
+        this.state = {
+            links: [],
+        }
     }
+
+    saveLink() {
+        const input = document.getElementById("linkLandInput");
+        const link = input.value;
+        this.setState({
+            links: [...this.state.links, link]
+        });
+        input.value = '';
     render() {
         const {
 
         } = this.props;
         return (
             <div className="link-land">
-                <input />
+                <input id="linkLandInput"/>
                 <div className="link-land-buttons">
-                    <button> Save </button>
+                    <button onClick={this.saveLink}> Save </button>
                     <button> Get </button>
                     <button> Clear </button>
                 </div>
