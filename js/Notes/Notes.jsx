c8d74e29f0b7f89129a69b20c089b3f17017bd0c
Jason Liang <jliang@collab.net>
2018-08-09 14:09:16 -0400

    link-land - reordering cards now reaoders notes as well
    -- defaultValue doesn't get updated on Update because textarea keeps internal state
    -- debounce updateTeamCardNotes so it doesn't fire constantly
    -- can't debounce onTextAreaChange because react recycles events after they are used
    ---- so unlike normal onChange, the debounced function can't use the event later
    -- It is considered bad practice to save props into state like this, but we need it to be in state since we can't debounce onTextAreaChange

--- js/Notes/Notes.jsx
+++ js/Notes/Notes.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import NotepadIcon from "../../icons/svgComponents/notepad-svg";
+import _ from 'underscore';
 
-class Notes extends React.Component {
-    constructor() {
-        super();
+class Notes extends React.PureComponent {
+    constructor(props) {
+        super(props);
         this.state = {
             isOpen: false,
-            iconChange: false
+            notes: props.notes
         };
         this.toggleOpen = this.toggleOpen.bind(this);
         this.onTextAreaChange = this.onTextAreaChange.bind(this);
+        this.saveTeamNotes = this.saveTeamNotes.bind(this);
+        this.saveTeamNotes = _.debounce(this.saveTeamNotes, 500)
     }
 
     toggleOpen() {
@@ -18,16 +21,27 @@ class Notes extends React.Component {
         });
     }
 
-    onTextAreaChange(event) {
-        const notes = event.target.value;
+    saveTeamNotes() {
+        const notes = this.state.notes;
         this.props.updateTeamCardNotes(this.props.teamName, notes)
     }
 
+    componentDidUpdate(oldProps) {
+        if (oldProps.notes !== this.props.notes) {
+            this.setState({
+                notes: this.props.notes,
+            })
+        }
+    }
+
+    onTextAreaChange(event) {
+        this.saveTeamNotes();
+        this.setState({
+            notes: event.target.value,
+        })
+    }
+
     render() {
-        const{
-            updateTeamCardNotes,
-            notes
-        } = this.props;
         const textareaStyles = this.state.isOpen ? {} : {display: "none"};
         const noteClass = this.props.notes ? "hasNotes" : "noNotes";
         return (
@@ -35,7 +49,7 @@ class Notes extends React.Component {
                 <span onClick={this.toggleOpen}>
                     <NotepadIcon className={noteClass} />
                 </span>
-                <textarea className="noteText" style={textareaStyles} onBlur={this.onTextAreaChange} defaultValue={notes} />
+                <textarea className="noteText" style={textareaStyles} onChange={this.onTextAreaChange} value={this.state.notes} />
             </span>
         );
     }
