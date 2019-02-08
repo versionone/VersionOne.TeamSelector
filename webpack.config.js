0b7698f0cf0d954f088e0e1adb75d860ec566065
Matt Higgins <mhhiggins@gmail.com>
2019-02-08 13:40:29 -0500

    fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("devlopment")
+        NODE_ENV: JSON.stringify("development")
       }
     })
   ],
